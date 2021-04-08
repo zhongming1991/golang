f1f1cf546e9d0bcea5f17f68229b870a1dc30fd4
zhongming1991 <406224709@qq.com>
2021-04-08 10:13:53 +0800

    A

--- 14.日志文件/main.go
+++ 14.日志文件/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"io"
+	"net/http"
 	"os"
 )
 
@@ -13,10 +14,10 @@ func main() {
 	gin.DefaultWriter = io.MultiWriter(file)
 
 	// 如果需要同时将日志写入文件和控制台，请使用以下代码。
-	//gin.DefaultWriter = io.MultiWriter(file, os.Stdout)
+	// gin.DefaultWriter = io.MultiWriter(file, os.Stdout)
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
+		c.String(http.StatusOK, "pong")
 	})
 	r.Run()
 }
